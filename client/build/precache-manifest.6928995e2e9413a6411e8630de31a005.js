458c19bf56d6f6fc33a333c85129506567f79c94
Katie Vogel <kvog983@gmail.com>
2021-01-18 19:05:34 -0500

    ejected create-react-app

---
+++ client/build/precache-manifest.6928995e2e9413a6411e8630de31a005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d73de07999ff64b2cbfa",
+    "url": "/static/css/main.934d515e.chunk.css"
+  },
+  {
+    "revision": "d73de07999ff64b2cbfa",
+    "url": "/static/js/main.6bc60955.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "35f4bbcbf68d13e8c201",
+    "url": "/static/css/2.15c77ce0.chunk.css"
+  },
+  {
+    "revision": "35f4bbcbf68d13e8c201",
+    "url": "/static/js/2.5014f01e.chunk.js"
+  },
+  {
+    "revision": "a3a18bca0d8eb29c9edbcd5766b79c6f",
+    "url": "/index.html"
+  }
+];
